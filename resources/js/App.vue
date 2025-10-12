1619d2b63ffb5a00a39eed36cbfe319266daa3fe
Dimas Fajar Ramadhan <66987497+dimasfr@users.noreply.github.com>
2025-10-12 20:22:06 +0700

    chore: initial project setup with Laravel & Vue
    - Added Laravel project structure
    - Added Vue 3 + Vite configuration
    - Added tailwindcss + shadcn configuration

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center mt-10">
+    <h1>Hello from Vue 3 + Laravel 🎉</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  color: #42b883;
+}
+</style>
